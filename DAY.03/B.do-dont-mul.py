3671a3ab35da992654ba65c31e5302cc90f99f69
Fio <44119871+fiorescarlatto@users.noreply.github.com>
2024-12-07 10:09:54 +0100

    DAY 03
    Added day 3 challenge and solutions

---
+++ DAY.03/B.do-dont-mul.py
@@ -0,0 +1,62 @@
+'''
+--- Part Two ---
+As you scan through the corrupted memory, you notice that some of the 
+conditional statements are also still intact. If you handle some of the 
+uncorrupted conditional statements in the program, you might be able to get 
+an even more accurate result.
+
+There are two new instructions you'll need to handle:
+
+  - The do() instruction enables future mul instructions.
+  - The don't() instruction disables future mul instructions.
+
+Only the most recent do() or don't() instruction applies. At the beginning 
+of the program, mul instructions are enabled.
+
+For example:
+
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+
+This corrupted memory is similar to the example from before, but this time 
+the mul(5,5) and mul(11,8) instructions are disabled because there is a 
+don't() instruction before them. The other mul instructions function 
+normally, including the one at the end that gets re-enabled by a do() 
+instruction.
+
+This time, the sum of the results is 48 (2*4 + 8*5).
+
+Handle the new instructions; what do you get if you add up all of the 
+results of just the enabled multiplications?
+'''
+
+# READS THE WHOLE FILE INTO A SINGLE STRING
+file = open('input.txt', 'r', encoding='utf-8')
+memory = file.read()
+file.close()
+
+import re
+# USING REGULAR EXPRESSIONS TO FIND mul(n,n)|do()|don't()
+regexp = "mul\\([0-9][0-9]?[0-9]?,[0-9][0-9]?[0-9]?\\)|do\\(\\)|don't\\(\\)"
+instructions = re.findall(regexp, memory)
+
+# KEEP TRACK OF THE DO() STATE
+execute = True
+result = 0
+
+# CALCULATES EACH MULT
+for i in instructions:
+    if i == 'do()':
+        execute = True
+    elif i == "don't()":
+        execute = False
+
+    elif execute:
+        # NORMALIZES THE mul(n,n) INSTRUCTION TO A [list] OF [int]
+        i = i.replace('mul(', '')
+        i = i.replace(')', '')
+        i = [int(x) for x in i.split(',')]
+
+        # CALCULATES THE mul INSTRUCTION
+        result += i[0] * i[1]
+
+print(result) # 98729041
