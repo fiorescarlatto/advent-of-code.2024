844421f32734c64517d57645c712b31dbe464ab3
Fio <44119871+fiorescarlatto@users.noreply.github.com>
2024-12-15 00:40:48 +0100

    Updated B.tree-finder.py
    Optimized the speed by changing the tree-likeness function.

--- DAY.14/B.tree-finder.py
+++ DAY.14/B.tree-finder.py
@@ -8,71 +8,72 @@ the robots should arrange themselves into a picture of a Christmas tree.
 What is the fewest number of seconds that must elapse for the robots to 
 display the Easter egg?
 '''
+
 # READ FILE INPUT LINE BY LINE
-file = open('input.txt', 'r', encoding='utf-8')
-lines = file.readlines()
-file.close()
+with open('input.txt', 'r', encoding='utf-8') as file:
+    lines = file.readlines()
+
+# OTHER INFORMATION
+size = [101, 103]
 
 # NORMALIZE THE INPUT
 robots = []
 
 for l in lines:
     l = l.strip().split(' ')
-    position = l[0][2:].split(',')
-    velocity = l[1][2:].split(',')
-    position = [int(p) for p in position]
-    velocity = [int(v) for v in velocity]
-    robots.append([position,velocity])
+    start = l[0][2:].split(',')
+    speed = l[1][2:].split(',')
 
-# OTHER INFORMATION
-size = [101, 103]
+    start = [int(s) for s in start]
+    speed = [int(s) for s in speed]
+    pos   = [0, 0]
+
+    robots.append( [start,speed,pos] )
 
 
-def treeness(room):
+def treeness(robots, room):
     treeness = 0
-    # JUST COUNTS THE NEIGHBOURS
-    for row in range(1, len(room) - 1):
-        for col in range(1, len( room[row] ) - 1):
-            if room[row][col] > 0:
-                treeness += 1 if room[row-1][col-1] > 0 else 0
-                treeness += 1 if room[row-1][col-0] > 0 else 0
-                treeness += 1 if room[row-1][col+1] > 0 else 0
-                treeness += 1 if room[row-0][col-1] > 0 else 0
-                treeness += 1 if room[row-0][col+1] > 0 else 0
-                treeness += 1 if room[row+1][col-1] > 0 else 0
-                treeness += 1 if room[row+1][col-0] > 0 else 0
-                treeness += 1 if room[row+1][col+1] > 0 else 0
+    # COUNT THE NEIGHBOURS
+    for r in robots:
+        col,row = r[2]
+        if row > 1 and row < len(room)-1:
+            if col > 1 and col < len(room[row])-1:
+                treeness += room[row-1][col-1]
+                treeness += room[row-1][col-0]
+                treeness += room[row-1][col+1]
+                treeness += room[row-0][col-1]
+                treeness += room[row-0][col+1]
+                treeness += room[row+1][col-1]
+                treeness += room[row+1][col-0]
+                treeness += room[row+1][col+1]
     return treeness
 
-def room_state(size, seconds, robots):
+def move(robots, time):
     room = []
-    # CREATE EMPTY ROOM
     for _ in range(size[1]):
         room.append( [0]*size[0] )
-    # ADD ROBOTS TO THE ROOM
     for r in robots:
-        position, velocity = r
-        goal = [
-            (position[0] + seconds * velocity[0]) % size[0], 
-            (position[1] + seconds * velocity[1]) % size[1]
-        ]     
-        room[ goal[1] ][ goal[0] ] += 1
+        start, speed, _ = r
+        pos = [ (start[0] + time * speed[0]) % size[0],
+                (start[1] + time * speed[1]) % size[1] ]
+        r[2] = pos
+        room[pos[1]][pos[0]] += 1
     return room
 
 def draw(room):
     for row in room:
-        row = ['.' if r == 0 else str(r) for r in row]
+        row = ['O' if r else ' ' for r in row]
         print(''.join(row))
 
 
 # SOLUTION
 tree = []
 
-for seconds in range(10000):
-    room = room_state(size, seconds, robots)
-    tree.append( (treeness(room), seconds) )
+for seconds in range( size[0]*size[1] ):
+    room = move(robots, seconds)
+    tree.append( (treeness(robots, room),seconds) )
 
 _, seconds = max(tree)
 
-draw( room_state(size, seconds, robots) )
+draw( move(robots, seconds) )
 print(seconds) # 6620
